6f75434c2f95bb75016afe7b6b2c76bca57604d6
Geurtsy <36181392+Geurtsy@users.noreply.github.com>
2018-06-10 01:52:52 +0800

    Finished simple scene change effect + Imported custom movement package

--- TheId/Assets/Tests/SimpleMove.cs
+++ TheId/Assets/Tests/SimpleMove.cs
@@ -1,61 +1,61 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class SimpleMove : MonoBehaviour {
+//public class SimpleMove : MonoBehaviour {
 
-    [SerializeField] private float _speed;
-    [SerializeField] private float _moveColourChangeDelay;
-    [SerializeField] private Material[] _moveMaterials;
-    [SerializeField] private Material _stillMaterial;
-    private Renderer _myRenderer;
+//    [SerializeField] private float _speed;
+//    [SerializeField] private float _moveColourChangeDelay;
+//    [SerializeField] private Material[] _moveMaterials;
+//    [SerializeField] private Material _stillMaterial;
+//    private Renderer _myRenderer;
 
-    private void Start()
-    {
-        _myRenderer = gameObject.GetComponent<Renderer>();
+//    private void Start()
+//    {
+//        _myRenderer = gameObject.GetComponent<Renderer>();
 
-        StartCoroutine(ChangeMovementColour());
-    }
+//        StartCoroutine(ChangeMovementColour());
+//    }
 
-    private void Update()
-    {
-        if(Input.GetKey(KeyCode.W))
-        {
-            transform.position += transform.forward * _speed * Time.deltaTime;
-        }
-        else if(Input.GetKey(KeyCode.S))
-        {
-            transform.position += -transform.forward * _speed * Time.deltaTime;
-        }
-        else if(Input.GetKey(KeyCode.A))
-        {
-            transform.position += -transform.right * _speed * Time.deltaTime;
-        }
-        else if(Input.GetKey(KeyCode.D))
-        {
-            transform.position += transform.right * _speed * Time.deltaTime;
-        }
-    }
+//    private void Update()
+//    {
+//        if(Input.GetKey(KeyCode.W))
+//        {
+//            transform.position += transform.forward * _speed * Time.deltaTime;
+//        }
+//        else if(Input.GetKey(KeyCode.S))
+//        {
+//            transform.position += -transform.forward * _speed * Time.deltaTime;
+//        }
+//        else if(Input.GetKey(KeyCode.A))
+//        {
+//            transform.position += -transform.right * _speed * Time.deltaTime;
+//        }
+//        else if(Input.GetKey(KeyCode.D))
+//        {
+//            transform.position += transform.right * _speed * Time.deltaTime;
+//        }
+//    }
 
-    private IEnumerator ChangeMovementColour()
-    {
-        while(true)
-        {
-            while(Input.anyKey)
-            {
-                foreach(Material mat in _moveMaterials)
-                {
-                    _myRenderer.material = mat;
-                    yield return new WaitForSeconds(_moveColourChangeDelay);
-                }
-            }
+//    private IEnumerator ChangeMovementColour()
+//    {
+//        while(true)
+//        {
+//            while(Input.anyKey)
+//            {
+//                foreach(Material mat in _moveMaterials)
+//                {
+//                    _myRenderer.material = mat;
+//                    yield return new WaitForSeconds(_moveColourChangeDelay);
+//                }
+//            }
 
-            if(!Input.anyKey)
-            {
-                _myRenderer.material = _stillMaterial;
-            }
+//            if(!Input.anyKey)
+//            {
+//                _myRenderer.material = _stillMaterial;
+//            }
 
-            yield return null;
-        }
-    }
-}
+//            yield return null;
+//        }
+//    }
+//}
---
+++ TheId/Assets/_Scripts/Effects/SceneChangeEffect.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneChangeEffect : MonoBehaviour{
+
+    [Header("Fade")]
+    [SerializeField] private Image _mask;
+    [SerializeField] private float _fadeDuration;
+    [SerializeField] private Color _colourEnd;
+
+    [Header("Effects")]
+    [Range(0.0f, 1.0f)]
+    [SerializeField] private float _particleStartOnPercentFade;
+    [SerializeField] private GameObject _fastEffect;
+    [SerializeField] private int _amountOfFastEffects;
+    [SerializeField] private GameObject _slowEffect;
+    [SerializeField] private float _durationBetweenFastParticles;
+    private bool _ready = true;
+    private int _currentEffectNum;
+
+    private void Start()
+    {
+        StartCoroutine(ProduceEffect());
+    }
+
+    private IEnumerator ProduceEffect()
+    {
+        Color beginColour = _mask.color;
+        float step = 0.0f;
+
+        while(step < 1.0f)
+        {
+            step += Time.deltaTime / _fadeDuration;
+            _mask.color = Color.Lerp(beginColour, _colourEnd, step);
+
+            if(step > _particleStartOnPercentFade && _ready)
+            {
+                StartCoroutine(CreateParticleEffect());
+            }
+
+            yield return null;
+        }
+
+
+        yield return null;
+    }
+
+    private IEnumerator CreateParticleEffect()
+    {
+        Vector2 randomPositionOnScreen = Camera.main.ViewportToWorldPoint(new Vector2(Random.value, Random.value));
+
+
+        if(_currentEffectNum < _amountOfFastEffects)
+        {
+            Instantiate(_fastEffect, randomPositionOnScreen, Quaternion.Euler(0, 0, 0));
+            _currentEffectNum++;
+            _ready = false;
+
+            yield return new WaitForSeconds(_durationBetweenFastParticles);
+
+            _ready = true;
+        }
+        else
+        {
+            Instantiate(_slowEffect, new Vector2(Camera.main.rect.width / 2, Camera.main.rect.height / 2), Quaternion.Euler(0, 0, 0));
+            _ready = false;
+        }
+
+        yield return null;
+    }
+}
